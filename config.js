533de714ab02fa2f5dbf3159d0151af64355fbbb
mrp1324536u7 <107055388+mrp1324536u7@users.noreply.github.com>
2022-06-08 06:38:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,19 @@
 const CONFIG = {
     titleWeb: "bùn",
     introTitle: 'Nguyễn Trần Hoàng Linh',
-    introDesc: `Chào cậu`,
-    btnIntro: '^^j^^',
+    introDesc: `Chào cậu!!!Tớ có chuỵn này mún nói với cậu`,
+    btnIntro: 'j',
     title: 'Cậu có iu mik hong',
     desc: '???? ',
     btnYes: 'iu lắm ',
     btnNo: 'Không nha :3',
-    question: 'Thế thì tại sao ko rep tn mik vại',
+    question: 'Trên thế giới có 7 tỉ người, tại sao cậu lại chọn mik vại <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: '',
-    
-    
+    reply: 'vì cậu đẹp trai',
+    mess:  'ui cậu quá khen 🥰',
+    messDesc: 'Thế thì rep tn mik từ tuần trc đi cậu ưi!!!',
+    btnAccept: 'OKI lun',
+  
     
     messLink: 'https://www.messenger.com/t/100075081210566' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
